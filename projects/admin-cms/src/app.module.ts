548b1751ac58690213874e6c255f52c6b60e5339
Phong Duong <s2pkmol@gmail.com>
2019-10-10 22:47:24 +0700

    Update pages

--- projects/admin-cms/src/app.module.ts
+++ projects/admin-cms/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectController } from './project/project.controller';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProjectModule],
+  controllers: [AppController, ProjectController],
   providers: [AppService],
 })
 export class AppModule {}
