5713d6f26491bfddd8dc7307f2fc8b4b3ab73f5d
Phong Duong <s2pkmol@gmail.com>
2019-11-13 22:28:29 +0700

    update edit project

--- projects/admin-cms/public/scripts/ts/new-project.ts
+++ projects/admin-cms/public/scripts/ts/new-project.ts
@@ -1,6 +1,10 @@
 import { POST, UPLOAD } from "./request";
-import { HTMLInputEvent, IScreenshot } from "./interfaces";
-import { getFieldValue } from "./helpers";
+import { HTMLInputEvent } from "./interfaces";
+import {
+  getFieldValue,
+  drawIcon,
+  drawScreenshotList,
+} from "./helpers";
 
 const screenshotsParentNode: Element = document.querySelector(
   "#screenshots__list"
@@ -73,75 +77,3 @@ document.querySelector(".new-form").addEventListener("submit", async e => {
     console.log(error);
   }
 });
-
-const drag = (e: HTMLInputEvent, url: IScreenshot) =>
-  e.dataTransfer.setData("text", JSON.stringify(url));
-
-const drop = (e: HTMLInputEvent, currentURL: IScreenshot) => {
-  e.preventDefault();
-  const newURL = JSON.parse(e.dataTransfer.getData("text"));
-  const newScreenshotURLS = screenshotURLs.map((url, index) => {
-    if (index === newURL.index) {
-      return currentURL.url;
-    }
-
-    if (index === currentURL.index) {
-      return newURL.url;
-    }
-
-    return url;
-  });
-  screenshotURLs = [...newScreenshotURLS];
-
-  drawScreenshotList(screenshotsParentNode, newScreenshotURLS);
-};
-
-const drawScreenshotList = (screenshotsNode: Element, urls: string[]) => {
-  while (screenshotsNode.firstChild) {
-    screenshotsNode.removeChild(screenshotsNode.firstChild);
-  }
-
-  urls.forEach((url, index) => {
-    const node = document.createElement("div");
-    const imgNode = document.createElement("img");
-
-    node.classList.add(
-      "col-6",
-      "col-md-3",
-      "screenshots__list__item",
-      `screenshots__list__item__${index}`
-    );
-    node.draggable = true;
-    imgNode.src = url;
-    imgNode.alt = "screenshot";
-
-    node.appendChild(imgNode);
-    screenshotsNode.appendChild(node);
-
-    document
-      .querySelector(`.screenshots__list__item__${index}`)
-      .addEventListener("dragstart", (e: HTMLInputEvent) =>
-        drag(e, { index, url })
-      );
-
-    document
-      .querySelector(`.screenshots__list__item__${index}`)
-      .addEventListener("drop", (e: HTMLInputEvent) => drop(e, { index, url }));
-
-    document
-      .querySelector(`.screenshots__list__item__${index}`)
-      .addEventListener("dragover", (e: HTMLInputEvent) => e.preventDefault());
-  });
-};
-
-const drawIcon = (iconNode: Element, url: string) => {
-  while (iconNode.firstChild) {
-    iconNode.removeChild(iconNode.firstChild);
-  }
-
-  const imgNode = document.createElement("img");
-  imgNode.classList.add("icon__item");
-  imgNode.src = url;
-  imgNode.alt = "screenshot";
-  iconNode.appendChild(imgNode);
-};
