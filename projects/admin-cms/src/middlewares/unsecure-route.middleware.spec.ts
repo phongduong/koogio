4383cb5c00e6ac919492a3f7251df9cd9416cb9c
Phong Duong <s2pkmol@gmail.com>
2019-12-17 20:04:32 +0700

    update husky

--- projects/admin-cms/src/middlewares/unsecure-route.middleware.spec.ts
+++ projects/admin-cms/src/middlewares/unsecure-route.middleware.spec.ts
@@ -1,7 +1,7 @@
-import { UnsecureRouteMiddleware } from './unsecure-route.middleware';
+import { UnsecureRouteMiddleware } from "./unsecure-route.middleware";
 
-describe('UnsecureRouteMiddleware', () => {
-  it('should be defined', () => {
+describe("UnsecureRouteMiddleware", () => {
+  it("should be defined", () => {
     expect(new UnsecureRouteMiddleware()).toBeDefined();
   });
 });
