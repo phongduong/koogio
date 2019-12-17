d48e58220e144a5e4c2b39c54da9d523b2b051b7
Phong Duong <s2pkmol@gmail.com>
2019-12-17 21:59:38 +0700

    update dotenv

--- projects/admin-cms/src/config/config.service.ts
+++ projects/admin-cms/src/config/config.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import * as dotenv from "dotenv";
-dotenv.config();
+
+if (process.env.NODE_ENV !== "production") dotenv.config();
 
 @Injectable()
 export class ConfigService {
