239aaeb2353292f93ab41edd4b636bdf48ddeee6
Phong Duong <phongdlh@bap.jp>
2020-01-07 17:33:13 +0700

    update project

---
+++ projects/koogio-website/src/components/ProjectItem.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  section.project-item
+    h2 {{ project.title }}
+    g-link(
+      to="/project/"
+    ) See more
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["project"],
+  mounted() {
+    console.log(this.project.project)
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
--- projects/koogio-website/src/layouts/Default.vue
+++ projects/koogio-website/src/layouts/Default.vue
@@ -1,50 +1,21 @@
-<template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+<template lang="pug">
+  .layout
+    header.header
+      nav.nav
+        g-link.nav__link(
+          to="/"
+        )
+          g-image.logo(
+            src="~/favicon.png" 
+            width="32px" 
+            height="32px" 
+          )
+    slot 
+    footer.footer
+      p Designed &amp; developed by KOOGIO
 </template>
 
 <static-query>
-query {
-  metadata {
-    siteName
-  }
-}
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+<style></style>
