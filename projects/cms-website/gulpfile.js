1c35135fc10373a3ff388680f593c7b79a11654c
Dương Lê Hoài Phong <phongdlh@bap.jp>
2020-02-11 09:15:14 +0700

    update script and optimize image

--- projects/cms-website/gulpfile.js
+++ projects/cms-website/gulpfile.js
@@ -5,6 +5,7 @@ const autoprefixer = require("autoprefixer");
 const purgecss = require("gulp-purgecss");
 const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
+const imagemin = require("gulp-imagemin");
 
 const cssDev = () => {
   return src("assets/style/style.css")
@@ -27,7 +28,9 @@ const cssProd = () => {
 };
 
 const copyImage = () => {
-  return src("assets/img/*.png").pipe(dest("_site/assets/img"));
+  return src("assets/img/*")
+    .pipe(imagemin())
+    .pipe(dest("_site/assets/img"));
 };
 
 exports.dev = parallel(cssDev, copyImage);
